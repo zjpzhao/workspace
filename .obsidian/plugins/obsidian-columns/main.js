b7cde124233522a6ec20a0be4fb1ddef0454e3d2
zjpzhao <33554063+zjpzhao@users.noreply.github.com>
2022-05-09 20:16:16 +0800

    vault backup: 2022-05-09 20:16:16
    Affected files:
    .obsidian/plugins/excalibrain/main.js
    .obsidian/plugins/excalibrain/manifest.json
    .obsidian/plugins/excalibrain/styles.css
    .obsidian/plugins/obsidian-columns/main.js
    .obsidian/plugins/obsidian-columns/manifest.json
    .obsidian/plugins/obsidian-columns/styles.css
    .obsidian/plugins/obsidian-excalidraw-plugin/data.json
    .obsidian/plugins/obsidian-excalidraw-plugin/main.js
    .obsidian/plugins/obsidian-excalidraw-plugin/manifest.json
    .obsidian/plugins/obsidian-git/main.js
    .obsidian/plugins/obsidian-git/manifest.json
    .obsidian/plugins/obsidian-kanban/manifest.json
    .obsidian/plugins/obsidian-kanban/styles.css
    .obsidian/plugins/remember-cursor-position/cursor-positions.json
    .obsidian/workspace

--- .obsidian/plugins/obsidian-columns/main.js
+++ .obsidian/plugins/obsidian-columns/main.js
@@ -55,26 +55,78 @@ var import_obsidian = __toModule(require("obsidian"));
 var COLUMNNAME = "col";
 var COLUMNMD = COLUMNNAME + "-md";
 var TOKEN = "!!!";
+var DEFAULT_SETTINGS = {
+  wrapSize: { value: 100, name: "Minimum width of column", desc: "Columns will have this minimum width before wrapping to a new row. 0 disables column wrapping. Useful for smaller devices" },
+  defaultSpan: { value: 1, name: "The default span of an item", desc: "The default width of a column. If the minimum width is specified, the width of the column will be multiplied by this setting." }
+};
+var parseBoolean = (value) => {
+  return value == "yes" || value == "true";
+};
+var parseObject = (value, typ) => {
+  if (typ == "string") {
+    return value;
+  }
+  if (typ == "boolean") {
+    return parseBoolean(value);
+  }
+  if (typ == "number") {
+    return parseFloat(value);
+  }
+};
+var processChild = (c) => {
+  if (c.firstChild != null && "tagName" in c.firstChild && c.firstChild.tagName == "BR") {
+    c.removeChild(c.firstChild);
+  }
+  let firstChild = c;
+  while (firstChild != null) {
+    if ("style" in firstChild) {
+      firstChild.style.marginTop = "0px";
+    }
+    firstChild = firstChild.firstChild;
+  }
+  let lastChild = c;
+  while (lastChild != null) {
+    if ("style" in lastChild) {
+      lastChild.style.marginBottom = "0px";
+    }
+    lastChild = lastChild.lastChild;
+  }
+};
 var ObsidianColumns = class extends import_obsidian.Plugin {
+  constructor() {
+    super(...arguments);
+    this.generateCssString = (span) => {
+      return "flex-grow:" + span.toString() + "; flex-basis:" + (this.settings.wrapSize.value * span).toString() + "px; width:" + (this.settings.wrapSize.value * span).toString() + "px";
+    };
+  }
   onload() {
     return __async(this, null, function* () {
+      yield this.loadSettings();
+      this.addSettingTab(new ObsidianColumnsSettings(this.app, this));
       this.registerMarkdownCodeBlockProcessor(COLUMNMD, (source, el, ctx) => {
         const sourcePath = ctx.sourcePath;
         let child = el.createDiv();
-        import_obsidian.MarkdownRenderer.renderMarkdown(source, child, sourcePath, null);
+        let renderChild = new import_obsidian.MarkdownRenderChild(child);
+        ctx.addChild(renderChild);
+        import_obsidian.MarkdownRenderer.renderMarkdown(source, child, sourcePath, renderChild);
       });
       this.registerMarkdownCodeBlockProcessor(COLUMNNAME, (source, el, ctx) => {
         const sourcePath = ctx.sourcePath;
-        let rows = source.split("\n");
         let child = createDiv();
-        import_obsidian.MarkdownRenderer.renderMarkdown(source, child, sourcePath, null);
+        let renderChild = new import_obsidian.MarkdownRenderChild(child);
+        ctx.addChild(renderChild);
+        import_obsidian.MarkdownRenderer.renderMarkdown(source, child, sourcePath, renderChild);
         let parent = el.createEl("div", { cls: "columnParent" });
         Array.from(child.children).forEach((c) => {
           let cc = parent.createEl("div", { cls: "columnChild" });
+          let renderCc = new import_obsidian.MarkdownRenderChild(cc);
+          ctx.addChild(renderCc);
+          cc.setAttribute("style", this.generateCssString(this.settings.defaultSpan.value));
           cc.appendChild(c);
+          processChild(c);
         });
       });
-      let processList = (element) => {
+      let processList = (element, context) => {
         for (let child of Array.from(element.children)) {
           if (child == null) {
             continue;
@@ -87,23 +139,28 @@ var ObsidianColumns = class extends import_obsidian.Plugin {
               continue;
             }
             if (!listItem.textContent.trim().startsWith(TOKEN + COLUMNNAME)) {
-              processList(listItem);
+              processList(listItem, context);
               continue;
             }
             child.removeChild(listItem);
             let colParent = element.createEl("div", { cls: "columnParent" });
+            let renderColP = new import_obsidian.MarkdownRenderChild(colParent);
+            context.addChild(renderColP);
             let itemList = listItem.querySelector("ul, ol");
             if (itemList == null) {
               continue;
             }
             for (let itemListItem of Array.from(itemList.children)) {
               let childDiv = colParent.createEl("div", { cls: "columnChild" });
+              let renderColC = new import_obsidian.MarkdownRenderChild(childDiv);
+              context.addChild(renderColC);
               let span = parseFloat(itemListItem.textContent.split("\n")[0].split(" ")[0]);
-              if (!isNaN(span)) {
-                childDiv.setAttribute("style", "flex-grow:" + span.toString());
+              if (isNaN(span)) {
+                span = this.settings.defaultSpan.value;
               }
+              childDiv.setAttribute("style", this.generateCssString(span));
               let afterText = false;
-              processList(itemListItem);
+              processList(itemListItem, context);
               for (let itemListItemChild of Array.from(itemListItem.childNodes)) {
                 if (afterText) {
                   childDiv.appendChild(itemListItemChild);
@@ -112,15 +169,45 @@ var ObsidianColumns = class extends import_obsidian.Plugin {
                   afterText = true;
                 }
               }
+              processChild(childDiv);
             }
           }
         }
       };
       this.registerMarkdownPostProcessor((element, context) => {
-        processList(element);
+        processList(element, context);
       });
     });
   }
   onunload() {
+  }
+  loadSettings() {
+    return __async(this, null, function* () {
+      this.settings = Object.assign({}, DEFAULT_SETTINGS, yield this.loadData());
+    });
+  }
+  saveSettings() {
+    return __async(this, null, function* () {
+      yield this.saveData(this.settings);
+    });
+  }
+};
+var ObsidianColumnsSettings = class extends import_obsidian.PluginSettingTab {
+  constructor(app, plugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+  display() {
+    const { containerEl } = this;
+    containerEl.empty();
+    containerEl.createEl("h2", { text: "Settings for obsidian-columns" });
+    let keyvals = Object.entries(DEFAULT_SETTINGS);
+    console.log(keyvals);
+    for (let keyval of keyvals) {
+      new import_obsidian.Setting(containerEl).setName(keyval[1].name).setDesc(keyval[1].desc).addText((text) => text.setPlaceholder(String(keyval[1].value)).setValue(String(this.plugin.settings[keyval[0]].value)).onChange((value) => {
+        keyval[1].value = parseObject(value, typeof keyval[1].value);
+        this.plugin.saveSettings();
+      }));
+    }
   }
 };
